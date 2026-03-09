8494e7b44f6f4e5e41b6874ddbc0ed016cd33929
sarah <sarahemmelin@gmail.com>
2026-03-09 22:28:05 +0100

    "added: canvas, index.html and first Ember test with some hard-coded values!"

--- ember.mjs
+++ ember.mjs
@@ -1,14 +1,43 @@
+import { SIZES, BASE_COLORS } from './constants.mjs';
+import Allele from './allele.mjs';
+
 class Ember {
     constructor(x,y){
         this.x = x;
         this.y = y;
-        this.size = 10;
-        this.color = "blue";
-        this.gender = Math.random() < 0.5 ? 'male' : 'female';
+        this.sizeAlleles = [
+            new Allele('baseSize', 
+            SIZES[Math.floor(Math.random() * SIZES.length)]),
+            new Allele('baseSize',
+            SIZES[Math.floor(Math.random() * SIZES.length)])
+        ];
+        this.colorAlleles = [
+            new Allele('baseColor', 
+            BASE_COLORS[Math.floor(Math.random() * BASE_COLORS.length)]),
+            new Allele('baseColor',
+            BASE_COLORS[Math.floor(Math.random() * BASE_COLORS.length)])
+        ];
+        this.saturationAlleles = [
+            new Allele('baseSaturation', Math.random()),
+            new Allele('baseSaturation', Math.random())
+        ];
+        this.glowAlleles = [
+            new Allele('baseGlow', Math.random()),
+            new Allele('baseGlow', Math.random())
+        ];
 
+        this.gender = Math.random() < 0.5 ? 'male' : 'female';
     }
 
+    draw(ctx){
+        ctx.shadowColor = 'crimson';
+        ctx.shadowBlur = 20;
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, 10, 0, Math.PI * 2);
+        ctx.fillStyle = 'crimson';
+        ctx.fill();
+    }
 
-}
+} 
 
 export default Ember;
--- game.mjs
+++ game.mjs
@@ -1,2 +1,10 @@
 import Ember from "./ember.mjs";
 
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const ember = new Ember(200,200);
+ember.draw(ctx);
