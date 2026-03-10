46627c6469657bd6e8591612dd43f91c8ac84e6b
sarah <sarahemmelin@gmail.com>
2026-03-10 23:19:56 +0100

    changed velocity depending on size, and also lifespan connected to size

--- constants.mjs
+++ constants.mjs
@@ -1,7 +1,11 @@
-export const SIZES = ['large', 'small'];
+export const SIZES = {
+    small: 7, 
+    large: 16
+};
+
 export const BASE_COLORS = {
     crimson:    {r: 230,    g: 57,  b:70},
     gold:       {r: 255,    g:215,  b: 0},
     violet:     {r: 155,    g: 93,  b: 229},
     blue:       {r: 0,      g: 180, b: 216} 
-}
+};
